1dc2cacd8295d6b1b5d3927e4902e533a16626fc
ferrerwensly <ferrerwensly@gmail.com>
2017-08-19 00:45:02 +0800

    merchant_id

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<div class="container">
+		<div class="card">
+			<input type="text" class="form-control" name="merchantid">
+		</div>
+	</div>
+</body>
+</html>
